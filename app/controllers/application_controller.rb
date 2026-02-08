d13b221b4a89caa6c1740176ac868dbcdff92519
frandle331 <s1240100@gmail.com>
2026-02-09 05:55:21 +0900

    Add household create and join flow with invitation code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,12 @@ class ApplicationController < ActionController::Base
   def after_sign_out_path_for(_resource_or_scope)
     root_path
   end
+
+  helper_method :current_household
+
+  def current_household
+    return nil unless current_user
+    current_user.households.order(:id).first
+  end
+
 end
